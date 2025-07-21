c70f379a352157e054021f69d2419c0235f62d22
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-21 11:55:16 +0530

    expense-vpc

--- output.tf
+++ output.tf
@@ -3,15 +3,7 @@ output "vpc_id" {
   
 }
 
-output "public_subnet_ids" {
-    value = aws_subnet.public[*].id
-  
-}
 
-output "private_subnet_ids" {
-    value = aws_subnet.private[*].id
-  
-}
 output "public_subnet_cidrs" {
     value = aws_subnet.public[*].id
   
